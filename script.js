e99e6b57fd81a59de8cfea6a7957a7b30ba42fb6
Oleg <olberd@bk.ru>
2025-06-22 18:07:00 +0300

    create index script

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Страница"
+let screens = "main"
+let screenPrice = 500
+let rollback = 400
+let fullPrice = 700
+let adaptive = "не понятно что здесь"
+
+alert(title)
+console.log(adaptive)
