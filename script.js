b15f5cd1b58968549b403875709e3eb3128d8fe3
olberd <olberd@bk.ru>
2025-06-28 12:55:04 +0300

    add discount

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = +prompt("Сколько это будет стоить?")
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "service2")
 let servicePrice2 = +prompt("Сколько это будет стоить?")
-
 let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let servicePercentPrice = fullPrice - fullPrice*(rollback/100)
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice*(rollback/100))
 
 console.log(title)
 console.log(screens)
@@ -20,4 +19,13 @@ console.log(adaptive)
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
 console.log(servicePercentPrice)
-console.log(fullPrice*(rollback/100))
+
+if (fullPrice >= 30000) {
+    console.log("Даем скидку в 10%")
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log("Даем скидку в 5%")
+} else if (fullPrice >= 0 && fullPrice < 15000) {
+    console.log("Скидка не предусмотрена")
+} else if (fullPrice < 0) {
+    console.log("Что-то пошло не так")
+}
