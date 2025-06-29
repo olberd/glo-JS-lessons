db09d9e211be87009bc99c2e593af9a8e3e18662
Oleg <olberd@bk.ru>
2025-06-29 11:41:01 +0300

    create lesson04

--- lesson04/script.js
+++ lesson04/script.js
@@ -5,13 +5,39 @@ let screens = prompt("Какие типы экранов нужно разраб
 let screenPrice = +prompt("Сколько будет стоить данная работа?")
 let rollback = 10
 let adaptive = confirm("Нужен ли адаптив на сайте?")
+let fullPrice
+let servicePercentPrice
+let allServicePrices
+
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "service1")
 let servicePrice1 = +prompt("Сколько это будет стоить?")
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "service2")
 let servicePrice2 = +prompt("Сколько это будет стоить?")
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let servicePercentPrice = Math.ceil(fullPrice - fullPrice*(rollback/100))
 
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable)
+}
+
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2
+}
+
+const getFullPrice = function () {
+    return screenPrice + allServicePrices
+}
+
+const getTitle = function (title) {
+    return title.trim().charAt(0).toUpperCase() + title.trim().toLowerCase().slice(1);
+}
+
+const getServicePercentPrices = function () {
+    return Math.ceil(fullPrice - fullPrice*(rollback/100))
+}
+
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+title = getTitle(title)
+servicePercentPrice = getServicePercentPrices()
 
 
 if (fullPrice >= 30000) {
@@ -24,7 +50,10 @@ if (fullPrice >= 30000) {
     console.log("Что-то пошло не так")
 }
 
-console.log(title)
+showTypeOf(allServicePrices)
+showTypeOf(fullPrice)
+showTypeOf(title)
+showTypeOf(servicePercentPrice)
 
 console.log(screens)
 console.log(fullPrice)
@@ -32,5 +61,3 @@ console.log(adaptive)
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
 console.log(servicePercentPrice)
-
-
